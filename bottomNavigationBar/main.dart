c3489ae07e6bf199113a37e43f71db15ef652271
Mahabub Ul Hasib Shawon <96023468+mahabubulhasibshawon@users.noreply.github.com>
2024-07-06 11:55:47 +0600

    Create main.dart

---
+++ bottomNavigationBar/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:navigation_project/pages/first_page.dart';
+import 'package:navigation_project/pages/home.dart';
+import 'package:navigation_project/pages/profile.dart';
+import 'package:navigation_project/pages/settings.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: FirstPage(),
+      routes: {
+        '/firstpage' : (context) => FirstPage(),
+        '/homepage' : (context) => HomePage(),
+        '/setttingspage'  : (context) => SettingsPage(),
+        '/profile' : (context) => ProfilePage()
+      },
+    );
+  }
+}
