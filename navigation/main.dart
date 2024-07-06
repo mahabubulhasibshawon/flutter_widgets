d4de5eff1117ed9a0560a647d4c1731b89676854
Mahabub Ul Hasib Shawon <96023468+mahabubulhasibshawon@users.noreply.github.com>
2024-07-06 11:02:44 +0600

    Create main.dart

---
+++ navigation/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:navigation_project/pages/first_page.dart';
+import 'package:navigation_project/pages/second_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: FirstPage(),
+      routes: {
+        '/firstpage' : (context) => FirstPage(),
+        '/secondpage' : (ocntext) => SecondPage()
+      },
+    );
+  }
+}
